f4e144316b97154c6aa315a2ef4096baa9c3d240
Briggs Elsperger <briggse@protonmail.com>
2017-08-18 20:15:41 -0500

    solved some

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
-#write your code here
+def echo(input)
+  return input
+end
+
+def shout(input)
+  return input.upcase
+end
+
+def repeat(input, amount=2)
+  return ((input + " ") * amount).strip
+end
+
+def start_of_word(word, number)
+  number = number - 1
+  return word[0..number]
+end
+
+def first_word(words)
+  return words.split[0]
+end
+
+def titleize(word)
+  return word.capitalize
+end
